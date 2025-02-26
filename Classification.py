570e4f9abcafa283258215d554f342f574cf3458
AnnaOccoffer <annaoccoffer@gmail.com>
2025-02-26 09:40:47 +0100

    create classification
    add import

---
+++ Classification.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plit
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report,confusion_matrix
+import seaborn as sns
+
+
+
